d82b43c6cb1c398cda93562d219d8c24374cfd9c
Nazilah17 <nazilah0000@gmail.com>
2023-12-04 03:53:12 +0800

    modified js

---
+++ akun/login.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let form = document.querySelector('.form');
+  
+    form.addEventListener('submit', function (event) {
+      event.preventDefault(); 
+  
+      let email = document.getElementById('email').value;
+      let password = document.getElementById('password').value;
+  
+      let userList = JSON.parse(localStorage.getItem('userList')) || [];
+  
+      let user = userList.find(user => user.email === email);
+  
+      if (!user) {
+        alert('E-mail tidak terdaftar');
+        return;
+      }
+  
+      if (user.password !== password) {
+        alert('Password yang dimasukkan salah');
+        return;
+      }
+  
+      localStorage.setItem('currentUser', JSON.stringify(user));
+
+      alert('Login berhasil');
+      window.location.href = '#.html';
+    });
+  });
+  
